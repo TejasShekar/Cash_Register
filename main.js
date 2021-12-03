8ab8d0c15dc1fc747bfcd9a4b6432be1f1f44e37
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-03 14:19:00 +0530

    update main.js
    Added calcualtion logic

--- main.js
+++ main.js
@@ -2,21 +2,25 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkBtn = document.querySelector("#check-btn");
 const errorMsg = document.querySelector("#error-msg");
-const noOfNotes = document.querySelector(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
+const notes = [2000, 500, 100, 20, 10, 5, 1];
 checkBtn.addEventListener("click", validate);
 
-function validate(){
-    errorMsg.style.display = "none";
-    if(billAmount.value > 0){
-        amountToBeGiven = cashGiven.value - billAmount.value;
-        calcAmount(amountToBeGiven);
-    }else{
-        errorMsg.style.display = "block";
-        errorMsg.innerText = "Invalid Bill Amount";
-    }
+function validate() {
+  errorMsg.style.display = "none";
+  if (billAmount.value > 0) {
+    amountToBeReturned = cashGiven.value - billAmount.value;
+    calcAmount(amountToBeReturned);
+  } else {
+    errorMsg.style.display = "block";
+    errorMsg.innerText = "Invalid Bill Amount";
+  }
 }
 
-function calcAmount(amountToBeGiven){
-    
+function calcAmount(amountToBeReturned) {
+  for(let i=0; i<notes.length; i++){
+    noOfNotes[i].innerHTML = amountToBeReturned / notes[i];
+    amountToBeReturned %= notes[i];
+  }
 }
