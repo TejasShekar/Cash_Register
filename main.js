e0b9a296080c654f33de177d72a09fed39eb2753
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-03 18:32:17 +0530

    Update main.js
    Fixed issue which was causing wrong output. Used Number() to convert  .value variables(which get converted to string due to use of .value) to numbers.

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const nextBtn = document.querySelector("#next-btn");
 const checkBtn = document.querySelector("#check-btn");
 const errorMsg = document.querySelector("#error-msg");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
-
 const notes = [2000, 500, 100, 20, 10, 5, 1];
 
 cashGiven.style.display = "none";
@@ -15,8 +14,9 @@ checkBtn.style.display = "none";
 nextBtn.addEventListener("click", proceed);
 checkBtn.addEventListener("click", validate);
 
+
 function proceed() {
-  if (billAmount.value > 0) {
+  if (Number(billAmount.value) > 0) {
     hideMessage();
     cashGivenText.style.display = "block";
     cashGiven.style.display = "initial";
@@ -29,22 +29,22 @@ function proceed() {
 
 function validate() {
   hideMessage();
-  if (billAmount.value > 0) {
+  if (Number(billAmount.value) > 0) {
     cashGivenText.style.display = "block";
     cashGiven.style.display = "initial";
-    if (cashGiven.value > billAmount.value) {
-      const amountToBeReturned = cashGiven.value - billAmount.value;
+    if (Number(cashGiven.value) > Number(billAmount.value)) {
+      const amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value);
       console.log(amountToBeReturned);
       calcAmount(amountToBeReturned);
-    } else if (cashGiven.value == 0) {
+    } else if (Number(cashGiven.value) == 0) {
       showMessage(
         "If no cash is given, then what do you want me to calculate 😑?"
       );
-    } else if (cashGiven.value == billAmount.value) {
+    } else if (Number(cashGiven.value) == Number(billAmount.value)) {
       showMessage("Just put the cash given into the register and chill");
-    } else if (cashGiven.value < 0) {
+    } else if (Number(cashGiven.value) < 0) {
       showMessage("Negative cash ?? Really ? Are you mentally stable ?");
-    } else if (billAmount.value > cashGiven.value) {
+    } else if (Number(billAmount.value) > Number(cashGiven.value)) {
       showMessage("Ask for more cash !");
     }
   } else {
